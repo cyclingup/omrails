f87aaaf12cbd284561d4242e39da0e94d369716d
Paul Kincaid-Smith <paul@sendgrid.com>
2013-03-23 21:55:37 -0600

    Add user model with Devise

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Omrails
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # required for devise on Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Omrails::Application.routes.draw do
+  devise_for :users
+
   get 'about' => 'pages#about'
   root :to => 'pages#home'
 
