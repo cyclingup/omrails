53e727b6242c82fac5af75f87a4ecbe8c89d3847
Paul Kincaid-Smith <paul@sendgrid.com>
2013-03-30 08:58:25 -0600

    Add Pins Associations (to users)

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,7 @@ class Pin < ActiveRecord::Base
   attr_accessible :description
 
   validates :description, presence: true
+
+  belongs_to :user
+  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  has_many :pins
 end
