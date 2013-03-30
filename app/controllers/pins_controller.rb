47c71155b6622d50b497693bf318c1f801435cd5
Paul Kincaid-Smith <paul@sendgrid.com>
2013-03-30 16:39:44 -0600

    Add jQuery Masonry

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,7 +4,7 @@ class PinsController < ApplicationController
   # GET /pins
   # GET /pins.json
   def index
-    @pins = Pin.all
+    @pins = Pin.order("created_at desc")
 
     respond_to do |format|
       format.html # index.html.erb
